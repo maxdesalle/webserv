d9bcd2808a8545630335d814f7418d32a226d60b
tderwedu <tderwedu@student.s19.be>
2022-02-03 09:41:57 +0100

    tderwedu: merged with tderwedu branch

--- HTTP_Config/Request.hpp
+++ HTTP_Config/Request.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Request.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldelmas <ldelmas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tderwedu <tderwedu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 11:47:16 by ldelmas           #+#    #+#             */
-/*   Updated: 2022/02/02 16:47:15 by ldelmas          ###   ########.fr       */
+/*   Updated: 2022/02/03 09:39:11 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,22 @@
 
 # include "Header.hpp"
 
+/*
+** TO DO
+**	- update constructors with new member variables
+*/
+
 class Request : public Header
 {
 	private:
+		enum body {LEN, CHUNKED, NONE};
+		enum state {STARTLINE, HEADERS, BODY, PROCESSING, DONE};
+		
 		static std::string const _fieldNames[22];
 		std::string		_method, _target, _version, _body;
+		Header			_headers;
+		body			_type;
+		state			_state;
 	public:
 		Request(void);
 		Request(Request const &src);
